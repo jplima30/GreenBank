101b2268487ba564c5d580af67e1a584dec6d87a
jplima30 <jplimadev@gmail.com>
2025-09-02 21:03:52 -0300

    changing login structures and visual details

--- GreenBank/ViewController.swift
+++ GreenBank/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginTextArea: UITextField!
+    @IBOutlet weak var keyTextArea: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        loginTextArea.layer.cornerRadius = 20
+        loginTextArea.layer.masksToBounds = true
+        loginTextArea.attributedPlaceholder = NSAttributedString(string: "E-mail",attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        
+        keyTextArea.layer.cornerRadius = 20
+        keyTextArea.layer.masksToBounds = true
+        keyTextArea.attributedPlaceholder = NSAttributedString(string: "Senha", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
     }
 
-
+    @IBAction func registerButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
